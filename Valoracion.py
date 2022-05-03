3ecca55b2197fcb44ecb336d9403b10899f6f623
Ananavarro1 <amnavarro96153@umanizales.edu.co>
2022-05-03 13:14:08 -0500

    funciones

---
+++ Valoracion.py
@@ -0,0 +1,8 @@
+class Valoracion():
+
+    def __init__(self,Calificacion=int,Comentario=str):
+        self.Calificacion = Calificacion
+        self.Comentario = Comentario
+
+    def CalificarServicio(self):
+        print("Comentario")
