e105344de881d64538f020ebef2eb0156c1d1b1f
Ananavarro1 <amnavarro96153@umanizales.edu.co>
2022-05-03 19:36:54 -0500

    clases

--- Factura.py
+++ Factura.py
@@ -1,11 +1,9 @@
-from Pedido import Pedido
-
-class Factura(Pedido, cliente):
+class Factura():
 
     def __init__(self, Cambio=float, Pago=float):
         self.Cambio = Cambio
         self.Pago = Pago
 
     def ComprobarVenta(self):
-        print("comprobar venta")
+        print("Venta realizada con exito")
         
