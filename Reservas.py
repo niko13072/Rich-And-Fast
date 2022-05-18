3a58d99041906dc35678f785d06ec13678ec0c44
Laurasruiz19 <lsruiz96337@umanizales.edu.co>
2022-05-17 21:33:42 -0500

    class

--- Reservas.py
+++ Reservas.py
@@ -1,10 +1,11 @@
 class Reservas():
-
-    def __init__(self, Fecha=str, Hora=float, NombreReserva=str, NumReserva=int):
+NumReserva;int
+    def __init__(self, Fecha:str, Hora:float, NombreReserva:str, usuario:Usuario):
        self.Fecha = Fecha
        self.Hora = Hora
        self.NombreReserva = NombreReserva
        self.NumReserva = NumReserva
+       self.Usuario = usuario
 
     def EnviarNotificacion(self):
         print("Notificacion reserva")
