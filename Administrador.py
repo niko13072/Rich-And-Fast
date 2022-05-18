dd4918ca2aa7c87c132043d5900a1fa76a3cdd1e
Laurasruiz19 <lsruiz96337@umanizales.edu.co>
2022-05-17 21:23:52 -0500

    class

--- Administrador.py
+++ Administrador.py
@@ -2,8 +2,9 @@ from Usuario import Usuario
 
 
 class Administrador(Usuario):
-
-    def __init__(self, Email=str):
+ 
+    def __init__(self,Id,password,Cargo,Nombre,Documento,Edad,Sexo,Email=str):
+        super().__init__(Id,password,Cargo,Nombre,Documento,Edad,Sexo)
         self.Email = Email
 
     def ActualizarProductos(self):
