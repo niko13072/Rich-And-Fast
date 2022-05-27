3ff35852ca733eb056d18408154fd366e65eaf65
Laurasruiz19 <lsruiz96337@umanizales.edu.co>
2022-05-27 00:52:54 -0500

    clases

--- Pedido.py
+++ Pedido.py
@@ -1,14 +1,16 @@
-
-
-
 class Pedido():
 
-    def __init__(self, NumeroPedido=int, Fecha=str, Total=float, NumeroMesa=int):
+    Fecha: str
+    NumeroPedido: int
+    def __init__(self, Total=float, NumeroMesa=int, PedidoCancelado=, PedidoListo=):
         self.NumeroPedido = NumeroPedido
         self.Fecha = Fecha
         self.Total = Total
         self.NumeroMesa = NumeroMesa
+        self.PedidoCancelado = PedidoCancelado
+        self.PedidoLisro = PedidoListo
 
+        
     def ModificarPedido(self):
         print("Seleccione el producto a modificar")
 
