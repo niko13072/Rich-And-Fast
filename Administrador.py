19b252964e6315aec299e382b7e9e397bcbe1742
niko13072 <nicolasosorioarias5@gmail.com>
2022-05-27 02:33:03 -0500

    actulizacion de meotdos

--- Administrador.py
+++ Administrador.py
@@ -3,8 +3,8 @@ from Usuario import Usuario
 
 class Administrador(Usuario):
  
-    def __init__(self,id,password,documento,email:str):
-        super().__init__(id,password,documento)
+    def __init__(self,id,contrasena,email:str):
+        super().__init__(id,contrasena)
         self.Email = email
     
     def AdministrarRestaurante(self):
