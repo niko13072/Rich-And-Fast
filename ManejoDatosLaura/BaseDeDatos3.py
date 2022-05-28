9a01a59586cf8c60e227eec5ff3d2fa00a2155a1
Laurasruiz19 <lsruiz96337@umanizales.edu.co>
2022-05-27 19:45:02 -0500

    base de datos

---
+++ ManejoDatosLaura/BaseDeDatos3.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+clase1 = pd.read_csv("Cocina.csv",header=0)
+clase2 = pd.read_csv("Pedido.csv",header=0)
+clase3 = pd.read_csv("MedioDePago.csv",header=0)
+clase4 = pd.read_csv("Ubicacion.csv",header=0)
+clase5 = pd.read_csv("Factura.csv",header=0)
