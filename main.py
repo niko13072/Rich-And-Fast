20a6a01881b53e9a4f91361b9abf497869ce0d09
niko13072 <nicolasosorioarias5@gmail.com>
2022-05-27 19:43:36 -0500

    main

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ import Pedido
 producto1 = Producto('Hamburguesa',13000)
 producto2 =Producto('Gaseosa',5000)
 
-list.append(Usuario('numeroMesa', 3))
-list.append(Usuario('pedido', str.Hamburguesa))
+usuario1=Usuario(10,'abc123')
+usuario1.Nombre='nicolas'
+
+usuario2=Usuario(11,'abc1234')
+usuario2.Nombre='ana'
+
+usuario3=Usuario(12,'abc12345')
+usuario3.Nombre='Laura'
+
