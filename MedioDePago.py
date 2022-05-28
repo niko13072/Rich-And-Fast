2b0e43e023491ecaaacb49b50b6dc21c01d502f8
Ananavarro1 <amnavarro96153@umanizales.edu.co>
2022-05-27 19:57:42 -0500

    main

--- MedioDePago.py
+++ MedioDePago.py
@@ -9,4 +9,6 @@ class MedioDePago():
     def PagarFactura(self):
         print("Confirmar el pago")
         
-
+if __name__=='__main__':
+    MedioDePago1=MedioDePago(158528466645)
+    print(MedioDePago1)
